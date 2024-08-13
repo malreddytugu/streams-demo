29b4f256f4ad68a6bfb6a7ee833149c3b6a7bcf9
malreddytugu <malreddytugu@gmail.com>
2024-08-13 21:10:34 +0530

    streams filter java8

--- src/com/javalearning/streams/Driver.java
+++ src/com/javalearning/streams/Driver.java
@@ -1,13 +1,24 @@
 package com.javalearning.streams;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import com.javalearning.streams.model.Employee;
 
 public class Driver {
 	public static void main(String[] args) {
 		
+		/*
 		String str= "Amar";
+
+		char ch = str.charAt(str.length()-1);
+		if(str.contains(String.valueOf(ch))) {
+			System.out.println(str+" ends with given character "+ch);
+		}else {
+			System.out.println(str+"doesn't ends with chaarcter "+ch);
+		}
+		*/
+		
 		List<Employee> employees = Employee.getEmployees();
 		//display all the employees in the List.
 		employees.stream()
@@ -20,13 +31,13 @@ public class Driver {
 				System.out.println(employee);
 			}
 		}
-		char ch = str.charAt(str.length()-1);
-		if(str.contains(String.valueOf(ch))) {
-			System.out.println(str+" ends with given character "+ch);
-		}else {
-			System.out.println(str+"doesn't ends with chaarcter "+ch);
-		}
 		
+		//using java8 streams
+		System.out.println("Employees whose Nme starts with 'K' using java8 streams ");
+		List<Employee> employeesWithNamesStartsK = employees.stream()
+			.filter(emp->emp.getName().contains("K"))
+			.collect(Collectors.toList());
+		employeesWithNamesStartsK.forEach(e->System.out.println(e));
 		
 	}
 
